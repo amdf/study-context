b358649f71946d1718f546b92c9508f6133e100e
amdf <787535+amdf@users.noreply.github.com>
2021-12-29 14:28:39 +0300

    timeout

--- main.go
+++ main.go
@@ -3,83 +3,68 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
-type key int
+type contextKey int
 
-const requestIDKey key = 0
+const userKey = contextKey(1)
 
-func newContextWithID(ctx context.Context, req *http.Request) context.Context {
-	ids, ok := req.URL.Query()["id"]
-	var id string
+var cnc map[string]context.CancelFunc
+
+func someFunc(w io.Writer, user string, ctx context.Context) {
+	t := time.NewTimer(10 * time.Second)
+
+	select {
+	case <-t.C:
+		fmt.Println(user, "finished...")
+	case <-ctx.Done():
+		fmt.Println(user, "canceled...")
+	}
+}
+
+func startHandler(w http.ResponseWriter, r *http.Request) {
+	user := r.Context().Value(userKey).(string)
+
+	someFunc(w, user, r.Context())
+}
+
+func stopHandler(w http.ResponseWriter, r *http.Request) {
+	user := r.Context().Value(userKey).(string)
+	cancel, ok := cnc[user]
 	if ok {
-		id = ids[0]
-	}
-	return context.WithValue(ctx, requestIDKey, id)
-}
-
-func getIDFromContext(ctx context.Context) string {
-	return ctx.Value(requestIDKey).(string)
-}
-
-type ContextHandler interface {
-	ServeHTTPContext(context.Context, http.ResponseWriter, *http.Request)
-}
-
-type ContextHandlerFunc func(context.Context, http.ResponseWriter, *http.Request)
-
-func (h ContextHandlerFunc) ServeHTTPContext(ctx context.Context, rw http.ResponseWriter, req *http.Request) {
-	h(ctx, rw, req)
-}
-
-func middleware(h ContextHandler) ContextHandler {
-	return ContextHandlerFunc(func(ctx context.Context, rw http.ResponseWriter, req *http.Request) {
-		ctx = newContextWithID(ctx, req)
-		h.ServeHTTPContext(ctx, rw, req)
-	})
-}
-
-type ContextAdapter struct {
-	ctx     context.Context
-	handler ContextHandler
-}
-
-func (ca *ContextAdapter) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	ca.handler.ServeHTTPContext(ca.ctx, rw, req)
-}
-
-func startHandler(ctx context.Context, rw http.ResponseWriter, req *http.Request) {
-	id := getIDFromContext(ctx)
-	if id == "" {
-		fmt.Fprintf(rw, "Start handler. Empty ID!\n")
-	} else {
-		fmt.Fprintf(rw, "Start handler. Request ID %s\n", id)
+		cancel()
 	}
 }
 
-func stopHandler(ctx context.Context, rw http.ResponseWriter, req *http.Request) {
-	id := getIDFromContext(ctx)
-	fmt.Fprintf(rw, "Stop handler. Request ID %s\n", id)
+func addUser(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		h(w, r.Clone(context.WithValue(r.Context(), userKey, r.FormValue("user"))))
+	}
+}
+
+func addTimeout(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user := r.Context().Value(userKey).(string)
+		if "" != user {
+			fmt.Println(user, "add timeout for user ", user)
+			ctx, cf := context.WithTimeout(r.Context(), time.Second)
+			cnc[user] = cf
+			h(w, r.Clone(ctx))
+		} else {
+			http.Error(w, "User unknown!", http.StatusInternalServerError)
+			return
+		}
+	}
 }
 
 func main() {
-	addr := ":8000"
-	start := &ContextAdapter{
-		ctx:     context.Background(),
-		handler: middleware(ContextHandlerFunc(startHandler)),
-	}
-	stop := &ContextAdapter{
-		ctx:     context.Background(),
-		handler: middleware(ContextHandlerFunc(stopHandler)),
-	}
-	mux := http.NewServeMux()
-
-	mux.Handle("/start", start)
-	mux.Handle("/stop", stop)
-
-	server := http.Server{Addr: addr, Handler: mux}
-	fmt.Println("starting server at", addr)
-	log.Fatal(server.ListenAndServe())
+	cnc = make(map[string]context.CancelFunc)
+	fmt.Println("start")
+	http.HandleFunc("/start", addUser(addTimeout(startHandler)))
+	http.HandleFunc("/stop", addUser(stopHandler))
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
