9e27af32c637660d8b7c6e15d38265caef020176
amdf <787535+amdf@users.noreply.github.com>
2021-12-29 16:44:45 +0300

    cancel

--- main.go
+++ main.go
@@ -18,11 +18,18 @@ var cnc map[string]context.CancelFunc
 func someFunc(w io.Writer, user string, ctx context.Context) {
 	t := time.NewTimer(10 * time.Second)
 
-	select {
-	case <-t.C:
-		fmt.Println(user, "finished...")
-	case <-ctx.Done():
-		fmt.Println(user, "canceled...")
+	for {
+		select {
+		case <-t.C:
+			fmt.Println(user, "finished...")
+			return
+		case <-ctx.Done():
+			fmt.Println(user, "canceled...")
+			return
+		default:
+			fmt.Print(".")
+			time.Sleep(200 * time.Millisecond)
+		}
 	}
 }
 
@@ -36,7 +43,10 @@ func stopHandler(w http.ResponseWriter, r *http.Request) {
 	user := r.Context().Value(userKey).(string)
 	cancel, ok := cnc[user]
 	if ok {
+		fmt.Println(user, "stopping...")
 		cancel()
+	} else {
+		fmt.Println(user, "not found (stopping)...")
 	}
 }
 
@@ -51,7 +61,22 @@ func addTimeout(h http.HandlerFunc) http.HandlerFunc {
 		user := r.Context().Value(userKey).(string)
 		if "" != user {
 			fmt.Println(user, "add timeout for user ", user)
-			ctx, cf := context.WithTimeout(r.Context(), time.Second)
+			ctx, cf := context.WithTimeout(r.Context(), 2*time.Second)
+			cnc[user] = cf
+			h(w, r.Clone(ctx))
+		} else {
+			http.Error(w, "User unknown!", http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func addCancel(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user := r.Context().Value(userKey).(string)
+		if "" != user {
+			fmt.Println(user, "add cancel for user ", user)
+			ctx, cf := context.WithCancel(r.Context())
 			cnc[user] = cf
 			h(w, r.Clone(ctx))
 		} else {
@@ -64,7 +89,8 @@ func addTimeout(h http.HandlerFunc) http.HandlerFunc {
 func main() {
 	cnc = make(map[string]context.CancelFunc)
 	fmt.Println("start")
-	http.HandleFunc("/start", addUser(addTimeout(startHandler)))
+	http.HandleFunc("/limit", addUser(addTimeout(startHandler)))
+	http.HandleFunc("/start", addUser(addCancel(startHandler)))
 	http.HandleFunc("/stop", addUser(stopHandler))
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
